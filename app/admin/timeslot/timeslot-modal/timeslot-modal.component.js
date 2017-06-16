bf8a7a04a0b58bf05441f769c2216bf35a269ef0
abhishek verma <abhissony@gmail.com>
2017-06-16 19:18:52 +0530

    validation on Timeslot Module

--- app/admin/timeslot/timeslot-modal/timeslot-modal.component.js
+++ app/admin/timeslot/timeslot-modal/timeslot-modal.component.js
@@ -5,22 +5,29 @@ function TimeslotModalController($state, TimeslotService) {
 	var ctrl = this;
 
 	ctrl.init = function(){
+		ctrl.currDate = function(){
+			var d = new Date();
+            var date = ctrl.formateDate(d);
+            return date;
+		}
 		ctrl.selectedDays = {};
 		ctrl.selectedAC = {};
 		ctrl.weekDayArr = ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday'];
 		ctrl.timeSlotArr = ['8am-10am', '10am-12pm', '12pm-2pm', '2pm-4pm', '4pm-6pm', '6pm-8pm'];
+		ctrl.checkCountSD = 0;
+		ctrl.checkCountTS = 0;
 	}
 
 	ctrl.save = function() {
 	  createTimeSlot();	
-      /*TimeslotService.createTimeSlotsRange(ctrl.resultFinal)
+      TimeslotService.createTimeSlotsRange(ctrl.resultFinal)
 			.then(function(result){
 				ctrl.modalInstance.close({action: "update"});
 			})
 			.catch(function(err){
 				console.log('Error Timeslot detail');
 				console.log(err);
-			});	*/
+			});	
 	}
 
 	ctrl.cancel = function(){
@@ -29,24 +36,54 @@ function TimeslotModalController($state, TimeslotService) {
 
 	ctrl.endDateChecker = function(){
 		ctrl.minDate = ctrl.startDate;
-		ctrl.endDate = ctrl.startDate;
+		if(ctrl.endDate < ctrl.startDate){
+			ctrl.endDate = "";
+		}
+		
 	}
 
 	ctrl.changedTS = function(n){
 		if(ctrl.selectedTS[n]){ //If it is checked
        		ctrl.selectedAC[n] = 0;
+       		ctrl.checkCountTS++;
    		}
    		else{
    			delete ctrl.selectedAC[n];
    			delete ctrl.selectedTS[n];
+   			ctrl.checkCountTS--;
    		}
 	}
+
 	ctrl.changedSD = function(weekDay){
 		if(ctrl.selectedDays[weekDay]){
-   		}
-   		else{
+			ctrl.checkCountSD++;
+   		}else{
    			delete ctrl.selectedDays[weekDay];
+   			ctrl.checkCountSD--;
    		}
+	}
+
+	ctrl.enableTS = function(){
+		if((ctrl.checkCountSD > 0) && (ctrl.endDate != "")){
+   			return true;
+   		}else{
+   			return false;
+   		}	
+	}
+
+	ctrl.enableSave = function(){
+		if((ctrl.checkCountTS > 0) && (ctrl.checkCountSD > 0) && (ctrl.endDate != "")){
+   			return true;
+   		}else{
+   			return false;
+   		}
+	}
+
+	ctrl.formateDate = function(date){
+		var date =	('0' + (date.getMonth()+1)).slice(-2) + '.'
+             		+ ('0' + date.getDate()).slice(-2) + '.'
+             		+ date.getFullYear();
+    	return date;
 	}
 
 	ctrl.init();
@@ -54,7 +91,7 @@ function TimeslotModalController($state, TimeslotService) {
 	// Creating Timeslot 
 	function createTimeSlot(){
 		ctrl.resultFinal=[];
-		ctrl.dateRange =[];
+		var dateRange =[];
 		var weekDay = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
 		
 		ctrl.startDatePicker = new Date(ctrl.startDate);
@@ -62,20 +99,18 @@ function TimeslotModalController($state, TimeslotService) {
 
 		while (ctrl.startDatePicker <= ctrl.endDatePicker) {
 						var day = weekDay[ctrl.startDatePicker.getDay()];
-						var date =   ('0' + (ctrl.startDatePicker.getMonth()+1)).slice(-2) + '.'
-             					   + ('0' + ctrl.startDatePicker.getDate()).slice(-2) + '.'
-             					   + ctrl.startDatePicker.getFullYear();
-						ctrl.dateRange.push({date:date,day:day});
+						var date = ctrl.formateDate(ctrl.startDatePicker);
+						dateRange.push({date:date,day:day});
                         ctrl.startDatePicker.setDate(ctrl.startDatePicker.getDate() + 1);
         }
 
-        console.log(ctrl.dateRange);
+        console.log(dateRange);
 
-		for (var i = 0; i < ctrl.dateRange.length; i++) {
+		for (var i = 0; i < dateRange.length; i++) {
          	angular.forEach(ctrl.selectedDays, function(value, key){
-				if(ctrl.dateRange[i].day == key){
+				if(dateRange[i].day == key){
 					angular.forEach(ctrl.selectedAC, function(value, key){
-						ctrl.resultFinal.push({availabilityCount:ctrl.selectedAC[key], date:ctrl.dateRange[i].date, timeslot:key});
+						ctrl.resultFinal.push({availabilityCount:ctrl.selectedAC[key], date:dateRange[i].date, timeslot:key});
 					});
 				}
 			});
